2ff74f2fcc50cccbb9ac9feb5f8d94d4e6b806de
Luis Manuel Brito <lsbrt4@gmail.com>
2020-02-20 14:31:34 -0400

    Estado final

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
@@ -20,10 +21,14 @@ module.exports = {
         }]
     },
     plugins: [
-        new HtmlWebpackPlugin([{
+        new HtmlWebpackPlugin({
             inject: true,
             template: './public/index.html',
             filename: './index.html'
+        }),
+        new CopyWebpackPlugin([{
+            from: './src/styles/styles.css',
+            to: ''
         }])
     ]
 }
